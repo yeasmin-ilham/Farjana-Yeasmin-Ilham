b14029d0832817b6cfe66cf07b12366f94a5ac5e
Yeasmin Ilham <farjanayeasminilham@gmail.com>
2026-04-17 11:06:30 +0600

    alignment problem solve

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ProductPage from "./components/Hero/ProductPage";
 export default function Home() {
   return (
     <main>
-      <div  className="min-h-screen max-w-7xl mx-42 bg-white">
+      <div  className="min-h-screen max-w-7xl mx-auto bg-white">
     <LandingPage/>
     <ProductPage/>
     </div>
